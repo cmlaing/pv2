7c0c459578ca1cd32db038decde64bcf497c4695
Crystal Laing <cistle@gmail.com>
2021-07-20 13:48:41 -0700

    Test
    test

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# pv2
+# portfolio
+
+Wireframe portfolio
+
+Some placeholder information from:
+<a href="https://www.oldbookillustrations.com/subjects/plants/">
+https://www.oldbookillustrations.com/subjects/plants/ </a>
+
+....
+
+TEST
  
---
+++ js/app.js
@@ -0,0 +1,24 @@
+/*==================================================================
+    
+    app.js
+    1. Includes JS plugin for sliding content on screen, taken 
+    from: https://github.com/mciastek/sal
+    Execute in this file by running: sal();
+    and including sal.js, sal.css
+    2. Bootstrap Carousel feature
+    https://getbootstrap.com/docs/5.0/components/carousel/
+
+==================================================================*/
+
+
+/* slide animation for each section */
+sal({
+    threshold: 0.3, /*makes the first 2 slides happen when the screen loads */
+  });
+
+sal();
+
+/* Carousel for Project cards*/
+
+var project1carousel = document.querySelector('#project1')
+var carousel = new bootstrap.Carousel(project1carousel)
